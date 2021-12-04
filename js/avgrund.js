b6f78d36330d24b72714b81726dbce7f80f87ba7
Roarnotes <Roarnotesfoundation@gmail.com>
2021-12-04 00:12:07 -1100

    popup card / section and dashboard post section finished with images of screenshot of dashboard progress added

--- js/avgrund.js
+++ js/avgrund.js
@@ -1,21 +1,32 @@
 (function($) {
   'use strict';
   $(function() {
-    $('#show').avgrund({
+    $("#show").avgrund({
       height: 500,
-      holderClass: 'custom',
+      holderClass: "custom",
       showClose: true,
-      showCloseText: 'x',
-      onBlurContainer: '.container-scroller',
-      template: '<p>So implement your design and place content here! If you want to close modal, please hit "Esc", click somewhere on the screen or use special button.</p>' +
-        '<div>' +
-        '<a href="http://twitter.com/voronianski" target="_blank" class="twitter btn btn-twitter btn-block">Twitter</a>' +
-        '<a href="http://dribbble.com/voronianski" target="_blank" class="dribble btn btn-dribbble btn-block">Dribbble</a>' +
-        '</div>' +
+      showCloseText: "x",
+      onBlurContainer: ".container-scroller",
+      template:
+        '<h4 class="card-title">Select Amount</h4>' +
+        '<p class="card-description">Recharge your meter fast and easy.</p>' +
+        '<form class="forms-sample">' +
+        '<div class="form-group">' +
+        "<tbody>" +
+        '<td><a type="button" class="btn btn btn-primary btn-rounded btn-fw" href="https://paystack.com/pay/scn-g9kka9">1,0000</a></td>' +
+        '<td><a type="button" class="btn btn btn-primary btn-rounded btn-fw" href="https://paystack.com/pay/scn-g9kka9">3,000</a></td>' +
+        '<td><a type="button" class="btn btn btn-primary btn-rounded btn-fw" href="https://paystack.com/pay/scn-g9kka9">5,000</a></td>' +
+        "</tbody>" +
+        "</div>" +
+        " <tbody>" +
+        '<td><a type="button" class="btn btn btn-primary btn-rounded btn-fw" href="https://paystack.com/pay/scn-g9kka9">10,000</a></td>' +
+        '<td><a type="button" class="btn btn btn-primary btn-rounded btn-fw" href="https://paystack.com/pay/scn-g9kka9">25,000</a></td>' +
+        " </tbody>" +
+        "</form>" +
         '<div class="text-center mt-4">' +
-        '<a href="#" target="_blank" class="btn btn-success me-2">Great!</a>' +
-        '<a href="#" target="_blank" class="btn btn-light">Cancel</a>' +
-        '</div>'
+        // '<a href="#" target="_blank" class="btn btn-success me-2">Great!</a>' +
+        '<a href="#" target="_blank" class="btn btn-primary">Cancel</a>' +
+        "</div>",
     });
   })
 })(jQuery);
